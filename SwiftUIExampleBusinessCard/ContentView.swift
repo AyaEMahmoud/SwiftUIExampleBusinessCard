0a849749b057a93fd62a73878d7dea23f81cdfff
Aya Mahmoud <ayamahmoud@Ayas-MacBook-Pro.local>
2023-12-09 19:44:23 +0200

    Add clipped image, add overlay

--- SwiftUIExampleBusinessCard/ContentView.swift
+++ SwiftUIExampleBusinessCard/ContentView.swift
@@ -12,10 +12,27 @@ struct ContentView: View {
         ZStack {
             Color(red: 0.44, green: 0.12, blue: 0.32, opacity: 1.00)
                 .ignoresSafeArea()
-            Text("Aya E Mahmoud")
-                .font(Font.custom("Pacifico-Regular", size: 44))
-                .bold()
+            VStack {
+                Image("aya")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 160.0, height: 160.0)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 6)
+                    )
+
+                Text("Aya E Mahmoud")
+                    .font(Font.custom("Pacifico-Regular", size: 44))
+                    .bold()
                 .foregroundColor(.white)
+                
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                
+                
+            }
         }
     }
 }
