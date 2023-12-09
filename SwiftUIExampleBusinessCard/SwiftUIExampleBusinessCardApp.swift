c38fc7feaedf9679aca1ed0025656769d81d4bd3
Aya Mahmoud <ayamahmoud@Ayas-MacBook-Pro.local>
2023-12-09 17:58:53 +0200

    Intial commit

---
+++ SwiftUIExampleBusinessCard/SwiftUIExampleBusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleBusinessCardApp.swift
+//  SwiftUIExampleBusinessCard
+//
+//  Created by Aya Mahmoud on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleBusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
